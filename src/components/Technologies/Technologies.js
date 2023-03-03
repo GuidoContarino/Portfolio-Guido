10561f11cceb92f0958552c6d0a22ec082116063
Guido Contarino <guidoconta11@gmail.com>
2023-03-03 14:57:28 -0300

    PortfolioTerminado

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,24 +1,49 @@
 import React from "react";
-import { DiFirebase, DiReact, DiZend } from "react-icons/di";
+import {
+  DiBootstrap,
+  DiCss3,
+  DiFirebase,
+  DiGithub,
+  DiHtml5,
+  DiJavascript1,
+  DiReact,
+  DiResponsive,
+  DiSass,
+  DiVisualstudio,
+} from "react-icons/di";
 import {
   Section,
   SectionDivider,
   SectionText,
   SectionTitle,
 } from "../../styles/GlobalComponents";
-import {
-  List,
-  ListContainer,
-  ListItem,
-  ListParagraph,
-  ListTitle,
-} from "./TechnologiesStyles";
+import { Container } from "./TechnologiesStyles";
 
 const Technologies = () => (
   <Section id="tech">
     <SectionDivider />
     <br />
     <SectionTitle>Skills</SectionTitle>
+    <SectionText>
+      Una de mis cosas favoritas desde que comencé a aprender a programar es
+      descubrir todas las increíbles herramientas, que los desarrolladores han
+      creado para facilitar su vida y la de otros desarrolladores. Estas son
+      algunas de las herramientas que uso con más frecuencia.
+    </SectionText>
+    <Container>
+      <DiHtml5 size="8rem" />
+      <DiCss3 size="8rem" />
+      <DiSass size="8rem" />
+      <DiBootstrap size="8rem" />
+      <DiJavascript1 size="8rem" />
+      <DiReact size="8rem" />
+      <DiFirebase size="8rem" />
+      <DiGithub size="8rem" />
+      <DiResponsive size="8rem" />
+      <DiVisualstudio size="8rem" />
+    </Container>
+    <br />
+    <SectionDivider />
   </Section>
 );
 
